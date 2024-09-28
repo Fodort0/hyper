fa66ee2bdbd18ca11d234bc5354eecce10347b4c
Fodor Tamás <141072145+Fodort0@users.noreply.github.com>
2024-09-28 15:29:21 +0200

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -8,7 +8,7 @@ module.exports = {
 		// or `'canary'` for less polished but more frequent updates
 		updateChannel: "stable",
 		// default font size in pixels for all tabs
-		fontSize: 14.4,
+		fontSize: 16,
 		// font family with optional fallbacks
 		fontFamily: '"JetBrainsMono Nerd Font", monospace',
 
